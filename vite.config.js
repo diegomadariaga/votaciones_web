24d126978ab25274e392ce996432b01c2d75d675
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-12-12 14:23:50 -0300

    feat: Add deployment scripts and gh-pages dependency to package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/votaciones_web/', // <- cambia 'votaciones' si tu repo tiene otro nombre
   plugins: [react()],
 })
